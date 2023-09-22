e34d88f51c6d65884438685a96d833351dd3fd0a
ben8169 <ben8169@outlook.kr>
2023-09-23 02:00:14 +0900

    Semi-completed code

--- assignment.c
+++ assignment.c
@@ -27,32 +27,73 @@ struct ipheader {
   struct  in_addr    iph_destip;   //Destination IP address
 };
 
+/* TCP Header */
+struct tcpheader {
+    u_short tcp_sport;               /* source port */
+    u_short tcp_dport;               /* destination port */
+    u_int   tcp_seq;                 /* sequence number */
+    u_int   tcp_ack;                 /* acknowledgement number */
+    u_char  tcp_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
+    u_char  tcp_flags;
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
+#define TH_ECE  0x40
+#define TH_CWR  0x80
+#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short tcp_win;                 /* window */
+    u_short tcp_sum;                 /* checksum */
+    u_short tcp_urp;                 /* urgent pointer */
+};
+
+
 void got_packet(u_char *args, const struct pcap_pkthdr *header,
                               const u_char *packet)
 {
   struct ethheader *eth = (struct ethheader *)packet;
 
   if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
-    struct ipheader * ip = (struct ipheader *)
-                           (packet + sizeof(struct ethheader)); 
+    struct ipheader *ip = (struct ipheader *)
+                           (packet + sizeof(struct ethheader));
+    if ((ip->iph_protocol) ==  IPPROTO_TCP){
+        // struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethheader) + sizeof (struct ipheader));
+        struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethheader) + ip->iph_ihl * 4);
 
-    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   
-    printf("         To: %s\n", inet_ntoa(ip->iph_destip));    
+        printf("======================================================\n");
+        printf("<<Ethernet Header>>\n");
+        printf("Src MAC addr: %02X:%02X:%02X:%02X:%02X:%02X\n", eth->ether_shost[0],eth->ether_shost[1],eth->ether_shost[2],eth->ether_shost[3],eth->ether_shost[4],eth->ether_shost[5]);
+        printf("Dst MAC addr: %02X:%02X:%02X:%02X:%02X:%02X\n", eth->ether_dhost[0],eth->ether_dhost[1],eth->ether_dhost[2],eth->ether_dhost[3],eth->ether_dhost[4],eth->ether_dhost[5]);
+        printf("\n");
+          
+        printf("<<IP Header>>\n");
+        printf("Src IP addr: %s\n", inet_ntoa(ip->iph_sourceip));   
+        printf("Dst IP addr: %s\n", inet_ntoa(ip->iph_destip));
+        printf("Identification: %d\n", ntohs(ip->iph_ident)); 
+        printf("\n");
 
-    /* determine protocol */
-    switch(ip->iph_protocol) {                                 
-        case IPPROTO_TCP:
-            printf("   Protocol: TCP\n");
-            return;
-        case IPPROTO_UDP:
-            printf("   Protocol: UDP\n");
-            return;
-        case IPPROTO_ICMP:
-            printf("   Protocol: ICMP\n");
-            return;
-        default:
-            printf("   Protocol: others\n");
-            return;
+        printf("<<TCP Header>>\n");
+        printf("Src Port: %d\n", ntohs(tcp->tcp_sport));   
+        printf("Dst port: %d\n", ntohs(tcp->tcp_dport));
+
+        printf("Flag(%x):",tcp->tcp_flags);
+        if (tcp->tcp_flags & 0x01)
+            printf("FIN  ");
+        if (tcp->tcp_flags & 0x02)
+            printf("SYN  ");
+        if (tcp->tcp_flags & 0x04)
+            printf("RST  ");
+        if (tcp->tcp_flags & 0x08)
+            printf("PSH  ");
+        if (tcp->tcp_flags & 0x10)
+            printf("ACK  ");
+        if (tcp->tcp_flags & 0x20)
+            printf("URG  ");
+        printf("\n\n");
+
     }
   }
 }
@@ -63,12 +104,12 @@ int main()
   pcap_t *handle;
   char errbuf[PCAP_ERRBUF_SIZE];
   struct bpf_program fp;
-  char filter_exp[] = "icmp";
+  char filter_exp[] = "tcp";
   bpf_u_int32 net;
 
   // Step 1: Open live pcap session on NIC with name enp0s3
-  // pcap open live 브서브티루티https://www.ibm.com/docs/ko/aix/7.3?topic=p-pcap-open-live-subroutine
-  handle = pcap_open_live("enp0s3", BUFSIZ, 1, 1000, errbuf);
+  // pcap open live 서브티루https://www.ibm.com/docs/ko/aix/7.3?topic=p-pcap-open-live-subroutine
+  handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, errbuf);
 
   // Step 2: Compile filter_exp into BPF psuedo-code
   pcap_compile(handle, &fp, filter_exp, 0, net);
